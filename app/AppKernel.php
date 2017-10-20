e7974423a59b660455a2e587f1edc379fc637a9d
Hugo Bidois <hugobidois@hotmail.fr>
2017-10-20 11:15:05 +0200

    Création du bundle base de données

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new FrontBundle\FrontBundle(),
+            new DataBaseBundle\DataBaseBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
---
+++ src/DataBaseBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace DataBaseBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('DataBaseBundle:Default:index.html.twig');
+    }
+}
