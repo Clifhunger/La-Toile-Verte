26738e298750f9531e738537f38827e72601fc6b
emmanuel cossard <emmanuelcossard85@gmail.com>
2017-10-25 11:10:13 +0200

    Release time line + bundle admin

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new FrontBundle\FrontBundle(),
             new DataBaseBundle\DataBaseBundle(),
+            new AdminBundle\AdminBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
