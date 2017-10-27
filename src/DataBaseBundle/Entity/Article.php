609a2bd74b5a304083f449b6901b7f949dc8bb5f
Rafael <rbetoull@etudiant.univ-lr.fr>
2017-10-27 17:09:00 +0200

    entité article

---
+++ src/DataBaseBundle/Entity/Article.php
@@ -0,0 +1,199 @@
+<?php
+
+namespace DataBaseBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Article
+ *
+ * @ORM\Table(name="article")
+ * @ORM\Entity(repositoryClass="DataBaseBundle\Repository\ArticleRepository")
+ */
+class Article
+{
+    /**
+     *
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="date")
+     *
+     */
+    protected $dateCreation;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     *
+     */
+    protected $title;
+
+    /**
+     * @ORM\Column(type="string", length=5096)
+     *
+     */
+    protected $description;
+
+    /**
+     * @ORM\Column(type="string", length=5096)
+     *
+     */
+    protected $detail;
+
+    /**
+     * @ORM\Column(type="integer")
+     *
+     */
+    protected $likes;
+
+    /**
+     * @ORM\Column(type="integer")
+     *
+     */
+    protected $comments;
+
+    /**
+     * @return mixed
+     */
+    public function getLikes()
+    {
+        return $this->likes;
+    }
+
+    /**
+     * @param mixed $likes
+     */
+    public function setLikes($likes)
+    {
+        $this->likes = $likes;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getComments()
+    {
+        return $this->comments;
+    }
+
+    /**
+     * @param mixed $comments
+     */
+    public function setComments($comments)
+    {
+        $this->comments = $comments;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDateCreation()
+    {
+        return $this->dateCreation;
+    }
+
+    /**
+     * @param mixed $dateCreation
+     */
+    public function setDateCreation($dateCreation)
+    {
+        $this->dateCreation = $dateCreation;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param mixed $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param mixed $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDetail()
+    {
+        return $this->detail;
+    }
+
+    /**
+     * @param mixed $detail
+     */
+    public function setDetail($detail)
+    {
+        $this->detail = $detail;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * @param mixed $image
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getVisible()
+    {
+        return $this->visible;
+    }
+
+    /**
+     * @param mixed $visible
+     */
+    public function setVisible($visible)
+    {
+        $this->visible = $visible;
+    }
+
+    /**
+     * @ORM\Column(type="string", length=5096)
+     *
+     */
+    protected $image;
+
+    /**
+     * @ORM\Column(type="integer")
+     *
+     */
+    protected $visible;
+}
