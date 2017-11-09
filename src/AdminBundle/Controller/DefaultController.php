33a11f6348ce17d13781d5f503e4ae0f288e8817
Rafael <rbetoull@etudiant.univ-lr.fr>
2017-11-09 11:06:26 +0100

    admin articles

--- src/AdminBundle/Controller/DefaultController.php
+++ src/AdminBundle/Controller/DefaultController.php
@@ -14,4 +14,35 @@ class DefaultController extends Controller
     {
         return $this->render('AdminBundle:Default:template.html.twig');
     }
+
+    /**
+     * @Route("/articles", name="adminArticles")
+     */
+    public function adminArticlesAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $articles = $em->getRepository('DataBaseBundle:Article')->findAll();
+        return $this->render('AdminBundle:Default:articles.html.twig', array('articles' => $articles));
+    }
+
+    /**
+     *  @Route("/adminArticle/id={id}", name="adminArticle")
+     */
+    public function adminArticleAction($id)
+    {
+        $repository = $this->getDoctrine()->getManager()->getRepository('DataBaseBundle:Article');
+        $article = $repository->find($id);
+
+        return $this->render('AdminBundle:Default:article.html.twig', array('article'=>$article));
+    }
+
+    /**
+     *  @Route("/createArticle", name="createArticle")
+     */
+    public function createArticleAction()
+    {
+        return $this->render('AdminBundle:Default:createArticle.html.twig');
+    }
+
+
 }
